61e13a356babc1d62417563079516e5b653d85df
ibacharnikov <ibacharnikov@cinimex.ru>
2021-09-27 16:30:33 +0500

    Refactoring: classes instead on fuctions (Conditioner page)

--- remote_cooling_android/lib/ui/conditioner_page.dart
+++ remote_cooling_android/lib/ui/conditioner_page.dart
@@ -87,12 +87,11 @@ class _ConditionerState extends State<ConditionerPage> {
         ],
       ),
       body: Builder(
-        builder: (ctx) => _ConditionerBody(
-          isLoading: isLoading,
-          conditioner: conditioner,
-          setMode: _setMode,
-        )
-      ),
+          builder: (ctx) => _ConditionerBody(
+                isLoading: isLoading,
+                conditioner: conditioner,
+                setMode: _setMode,
+              )),
     );
   }
 }
@@ -113,23 +112,33 @@ class _ConditionerBody extends StatelessWidget {
                         CircularProgressIndicator(color: Constants.mainOrange))
               ]
             : [
-              _getStatus(conditioner),
-                  Divider(
-                    height: 20,
-                    thickness: 5,
-                    indent: 20,
-                    endIndent: 20,
-                  ),
-                  _renderModeChoose(conditioner, setMode),
-                  Divider(
-                    height: 20,
-                    thickness: 5,
-                    indent: 20,
-                    endIndent: 20,
-                  ),
-                  Spacer(),
-                  _ConditionerFooter(conditioner: conditioner)
-            ]);
+                Text(
+                  _getTemperature(conditioner.status),
+                  style: TextStyle(fontSize: 100),
+                ),
+                Text(
+                  _getMode(conditioner.status),
+                  style: TextStyle(fontSize: 60),
+                ),
+                Divider(
+                  height: 20,
+                  thickness: 5,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+                _ConditionerModeChooser(
+                  conditioner: conditioner,
+                  onChange: setMode
+                ),
+                Divider(
+                  height: 20,
+                  thickness: 5,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+                Spacer(),
+                _ConditionerFooter(conditioner: conditioner)
+              ]);
   }
 }
 
@@ -140,17 +149,17 @@ class _ConditionerFooter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [_getEndpoint(conditioner), _getUpdated(conditioner)],
+      children: [
+        Text('API: '),
+        Text(conditioner.endpoint),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text('Обновлено: '),
+          Text(TimeAgo.timeAgoSinceDate(conditioner.updatedAt)),
+          Text(' пользователем ${conditioner.userName}')
+        ])
+      ],
     );
   }
-}
-
-Row _getUpdated(Conditioner conditioner) {
-  return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-    Text('Обновлено: '),
-    Text(TimeAgo.timeAgoSinceDate(conditioner.updatedAt)),
-    Text(' пользователем ${conditioner.userName}')
-  ]);
 }
 
 String _getStringStatus(ConditionerStatus status) {
@@ -170,16 +179,6 @@ String _getStringStatus(ConditionerStatus status) {
     default:
       return 'нет связи';
   }
-}
-
-Row _getEndpoint(Conditioner conditioner) {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Text('API: '),
-      Text(conditioner.endpoint),
-    ],
-  );
 }
 
 String _getTemperature(ConditionerStatus status) {
@@ -217,21 +216,6 @@ String _getMode(ConditionerStatus status) {
   }
 }
 
-Column _getStatus(Conditioner conditioner) {
-  return Column(
-    children: [
-      Text(
-        _getTemperature(conditioner.status),
-        style: TextStyle(fontSize: 100),
-      ),
-      Text(
-        _getMode(conditioner.status),
-        style: TextStyle(fontSize: 60),
-      ),
-    ],
-  );
-}
-
 bool _isConditionerOn(Conditioner conditioner) {
   switch (conditioner.status) {
     case ConditionerStatus.off:
@@ -246,45 +230,53 @@ bool _isConditionerOn(Conditioner conditioner) {
   }
 }
 
-Column _renderModeChoose(Conditioner conditioner, Function onChange) {
-  return Column(
-    children: [
-      ListTile(
-        title: Text(_getStringStatus(ConditionerStatus.auto)),
-        leading: Radio(
-            value: ConditionerStatus.auto,
-            groupValue: conditioner.status,
-            onChanged: (value) {
-              onChange(value);
-            }),
-      ),
-      ListTile(
-        title: Text(_getStringStatus(ConditionerStatus.cold17)),
-        leading: Radio(
-            value: ConditionerStatus.cold17,
-            groupValue: conditioner.status,
-            onChanged: (value) {
-              onChange(value);
-            }),
-      ),
-      ListTile(
-        title: Text(_getStringStatus(ConditionerStatus.cold22)),
-        leading: Radio(
-            value: ConditionerStatus.cold22,
-            groupValue: conditioner.status,
-            onChanged: (value) {
-              onChange(value);
-            }),
-      ),
-      ListTile(
-        title: Text(_getStringStatus(ConditionerStatus.fan)),
-        leading: Radio(
-            value: ConditionerStatus.fan,
-            groupValue: conditioner.status,
-            onChanged: (value) {
-              onChange(value);
-            }),
-      ),
-    ],
-  );
+class _ConditionerModeChooser extends StatelessWidget {
+  final Conditioner conditioner;
+  final Function onChange;
+  _ConditionerModeChooser({@required this.conditioner,
+  @required this.onChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(_getStringStatus(ConditionerStatus.auto)),
+          leading: Radio(
+              value: ConditionerStatus.auto,
+              groupValue: conditioner.status,
+              onChanged: (value) {
+                onChange(value);
+              }),
+        ),
+        ListTile(
+          title: Text(_getStringStatus(ConditionerStatus.cold17)),
+          leading: Radio(
+              value: ConditionerStatus.cold17,
+              groupValue: conditioner.status,
+              onChanged: (value) {
+                onChange(value);
+              }),
+        ),
+        ListTile(
+          title: Text(_getStringStatus(ConditionerStatus.cold22)),
+          leading: Radio(
+              value: ConditionerStatus.cold22,
+              groupValue: conditioner.status,
+              onChanged: (value) {
+                onChange(value);
+              }),
+        ),
+        ListTile(
+          title: Text(_getStringStatus(ConditionerStatus.fan)),
+          leading: Radio(
+              value: ConditionerStatus.fan,
+              groupValue: conditioner.status,
+              onChanged: (value) {
+                onChange(value);
+              }),
+        ),
+      ],
+    );
+  }
 }
