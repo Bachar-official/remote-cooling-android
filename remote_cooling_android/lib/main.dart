b7cb6d033b1d6ad35568172bd26200c5c6f14c08
ibacharnikov <ibacharnikov@cinimex.ru>
2022-01-11 11:09:53 +0500

    logging in colors

--- remote_cooling_android/lib/main.dart
+++ remote_cooling_android/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ansicolor/ansicolor.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
@@ -7,9 +8,18 @@ import 'package:remote_cooling_android/domain/model/conditioner-list-model.dart'
 import 'package:remote_cooling_android/domain/model/conditioner-model.dart';
 
 void main() async {
+  ansiColorDisabled = false;
+  final whitePen = AnsiPen()..white(bold: true);
+  final redPen = AnsiPen()..red(bold: true);
+  final bluePen = AnsiPen()..blue(bold: true);
   Logger.root.level = Level.ALL; // defaults to Level.INFO
   Logger.root.onRecord.listen((record) {
-    print('${record.loggerName}: ${record.level.name}: ${record.time}: ${record.message}');
+    String message = '${record.loggerName}: ${record.level.name}: ${record.time}: ${record.message}';
+    switch(record.level.name) {
+      case 'WARNING': print(redPen(message)); break;
+      case 'FINE': print(bluePen(message)); break;
+      default: print(whitePen(message));
+    }
   });
   await Hive.initFlutter();
   await Hive.openBox('settings');
