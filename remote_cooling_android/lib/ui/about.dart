cfad9300b5265cde8984c26e6dc74cb85035ee79
ibacharnikov <ibacharnikov@cinimex.ru>
2022-01-25 11:54:09 +0500

    Getting version from pubspec

--- remote_cooling_android/lib/ui/about.dart
+++ remote_cooling_android/lib/ui/about.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class AboutPage extends StatelessWidget{
+
+  Future<String> getVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    return packageInfo.version;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,8 +19,15 @@ class AboutPage extends StatelessWidget{
           padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
           child: Column(
             children: [
-              Text('Cinimex Cooling v.0.3.0'),
-              Text('Разработчик: Иван Бачарников'),
+              Text('Cinimex Cooling'),
+              FutureBuilder<String>(
+                  future: getVersion(),
+                  builder: (context, snapshot) {
+                    final data = snapshot.data;
+                    final text = data != null ? data : '';
+                    return Text('Version ${text}');
+                  }),
+              Text('Developer: Ivan Bacharnikov'),
               Text('Email: ibacharnikov@cinimex.ru'),
               Text('Github: https://github.com/Bachar_official'),
               Text('Telegram: @Bachar-official')
